97a628f21a6d55d7356a5bc0b7131fb47c3fedbc
Mast3rSoda <63344754+Mast3rSoda@users.noreply.github.com>
2022-05-08 23:11:18 +0200

    almost working

---
+++ biometria-7/Algorithm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biometria_7
+{
+    public static class Algorithm
+    {
+        //albo to ja jestem debilem, albo ten algorytm jest zjebany i nie ma racji bytu
+        //http://aragorn.pb.bialystok.pl/~zspinfo/arts/2001%20CAIP.pdf - imo caly ten papier jest do dupy... but what do I know
+        //NIE ROZUMIEM CO KURWA OZNACZA 2.1 ppkt. 3 - "Consider the points with 2, 3 or 4 sticking neighbors and change them into 4’s"
+        //"sticking neighbors???????????????????" - moze angielskiego niech sie naucza
+        //nie wazne z ktorej strony na to patrze, warunki wstawiania 4 sa losowe - bo czasem jest wstawiane a czasem nie
+        //po dalszej inspekcji tego papieru, w 2.1 ppkt. 2 sa bledy oznaczania pikseli xD.
+        //gowno obsrane - napisze sam lepiej
+        public static Bitmap KMM(Bitmap bmp)
+        {
+            int[] val = new int[] { 3, 5, 7, 12, 13, 14, 15, 20,
+                                21, 22, 23, 28, 29, 30, 31, 48,
+                                52, 53, 54, 55, 56, 60, 61, 62,
+                                63, 65, 67, 69, 71, 77, 79, 80,
+                                81, 83, 84, 85, 86, 87, 88, 89,
+                                91, 92, 93, 94, 95, 97, 99, 101,
+                                103, 109, 111, 112, 113, 115, 116, 117,
+                                118, 119, 120, 121, 123, 124, 125, 126,
+                                127, 131, 133, 135, 141, 143, 149, 151,
+                                157, 159, 181, 183, 189, 191, 192, 193,
+                                195, 197, 199, 205, 207, 208, 209, 211,
+                                212, 213, 214, 215, 216, 217, 219, 220,
+                                221, 222, 223, 224, 225, 227, 229, 231,
+                                237, 239, 240, 241, 243, 244, 245, 246,
+                                247, 248, 249, 251, 252, 253, 254, 255 };
+
+            byte[,] grayS = ImageTo2DByteArray(bmp);
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            byte[] vs = new byte[data.Stride * data.Height];
+            Marshal.Copy(data.Scan0, vs, 0, vs.Length);
+
+            bool c = false;
+            do
+            {
+                c = false;
+                for (int i = 0; i < data.Height; i++)
+                    for (int y = 0; y < data.Width; y++)
+                        grayS[i, y] = (byte)(grayS[i, y] == 0 ? 1 : 0);
+
+                for (int i = 0; i < data.Height; i++)
+                    for (int y = 0; y < data.Width; y++)
+                    {
+                        if (grayS[i, y] == 1)
+                        {
+                            if (i != 0 && grayS[i - 1, y] == 0)
+                                grayS[i, y] = 2;
+
+                            else if (i != data.Height - 1 && grayS[i + 1, y] == 0)
+                                grayS[i, y] = 2;
+
+                            else if (y != 0 && grayS[i, y - 1] == 0)
+                                grayS[i, y] = 2;
+
+                            else if (y != data.Width - 1 && grayS[i, y + 1] == 0)
+                                grayS[i, y] = 2;
+
+                            else if (i != 0 && y != 0 && grayS[i - 1, y - 1] == 0)
+                                grayS[i, y] = 3;
+
+                            else if (i != 0 && y != data.Width - 1 && grayS[i - 1, y + 1] == 0)
+                                grayS[i, y] = 3;
+
+                            else if (i != data.Height - 1 && y != 0 && grayS[i + 1, y - 1] == 0)
+                                grayS[i, y] = 3;
+
+                            else if (i != data.Height - 1 && y != data.Width - 1 && grayS[i + 1, y + 1] == 0)
+                                grayS[i, y] = 3;
+                        }
+                        else
+                            vs[(i * data.Stride) + y * 3] = 255;
+                    }
+
+                for (int n = 2; n < 4; n++)
+                    for (int i = 0; i < data.Height; i++)
+                        for (int y = 0; y < data.Width; y++)
+                        {
+                            if (grayS[i, y] == n)
+                                if (val.Contains(CalculateWeight(i, y, grayS, data.Width, data.Height)))
+                                {
+                                    grayS[i, y] = 0;
+                                    vs[(i * data.Stride) + (y * 3)] =
+                                    vs[(i * data.Stride) + (y * 3) + 1] =
+                                    vs[(i * data.Stride) + (y * 3) + 2] = byte.MaxValue;
+                                    c = true;
+                                }
+                                else
+                                    grayS[i, y] = 1;
+                        }
+            } while (c);
+            Marshal.Copy(vs, 0, data.Scan0, vs.Length);
+            bmp.UnlockBits(data);
+            return bmp;
+        }
+
+        //TODO make this work :/
+        public static int CalculateWeight(int i, int j, byte[,] grayS, int w, int h)
+        {
+            int[] N = new int[] { 128, 1, 2, 64, 0, 4, 32, 16, 8 };
+            int weight = 0;
+            if (i - 1 > 0 && j - 1 > 0 && grayS[i - 1, j - 1] != 0)
+                weight += N[0];
+            if (j - 1 > 0 && grayS[i, j - 1] != 0)
+                weight += N[1];
+            if (i + 1 < h && j - 1 > 0 && grayS[i + 1, j - 1] != 0)
+                weight += N[2];
+            if (i - 1 > 0 && grayS[i - 1, j] != 0)
+                weight += N[3];
+            if (i + 1 < h && grayS[i + 1, j] != 0)
+                weight += N[5];
+            if (i - 1 > 0 && j + 1 < w && grayS[i - 1, j + 1] != 0)
+                weight += N[6];
+            if (j + 1 < w && grayS[i, j + 1] != 0)
+                weight += N[7];
+            if (i + 1 < h && j + 1 < w && grayS[i + 1, j + 1] != 0)
+                weight += N[8];
+            return weight;
+        }
+
+        public static byte[,] ImageTo2DByteArray(Bitmap bmp)
+        {
+            int width = bmp.Width;
+            int height = bmp.Height;
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+            byte[] bytes = new byte[height * data.Stride];
+            try
+            {
+                Marshal.Copy(data.Scan0, bytes, 0, bytes.Length);
+            }
+            finally
+            {
+                bmp.UnlockBits(data);
+            }
+
+            byte[,] result = new byte[height, width];
+            for (int y = 0; y < height; ++y)
+                for (int x = 0; x < width; ++x)
+                {
+                    int offset = y * data.Stride + x * 3;
+                    result[y, x] = (byte)((bytes[offset + 0] + bytes[offset + 1] + bytes[offset + 2]) / 3);
+                }
+            return result;
+        }
+
+    }
+}
--- biometria-7/MainWindow.xaml.cs
+++ biometria-7/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +11,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +24,56 @@ namespace biometria_7
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Bitmap? sourceImage = null;
+        Bitmap? imageToEdit = null;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg;*.png)|*.jpg;*.png|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                imageToEdit = this.sourceImage = new Bitmap($"{fileName}");
+                Img.Source = ImageSourceFromBitmap(this.sourceImage);
+            }
+        }
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+
+        private void KMM_Click(object sender, RoutedEventArgs e)
+        {
+            if (sourceImage == null)
+            {
+                MessageBox.Show("You haven't uploaded any files", "Image error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            Bitmap bitmap = new Bitmap(this.sourceImage.Width, this.sourceImage.Height);
+            bitmap = (Bitmap)this.imageToEdit.Clone();
+            Img.Source = ImageSourceFromBitmap(Algorithm.KMM(bitmap));
+        }
+
+        private void K3M_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
